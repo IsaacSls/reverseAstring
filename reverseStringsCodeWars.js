16ff53d430abdb06ba2c1b96c04036cf2e8398f0
IsaacSls <147279446+IsaacSls@users.noreply.github.com>
2023-10-11 19:30:17 -0500

    Add files via upload
    Daily Codewars 
    This is my way of reversing a string. Simple and quick. I understand there is a longer way to do it.

---
+++ reverseStringsCodeWars.js
@@ -0,0 +1,9 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+// Reverse the string!!!
+
+function solution(str){
+    return str.split('').reverse().join('');
+  }
